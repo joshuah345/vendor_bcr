ce30bee720c5486c9a88886b350335c0bff37da0
hiroshi <hiroshi@tanuki.page>
2024-11-24 07:21:18 -0500

    Migrate to soong

--- bcr.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-
-# Include prebuilts packages
-include vendor/bcr/prebuilts/prebuilts.mk
-RELAX_USES_LIBRARY_CHECK=true
